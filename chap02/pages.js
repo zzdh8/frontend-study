1e8727c5dcc40eca0a1aa23b6ffe3f281527d80f
Hyun jinyong <ekwls20@gmail.com>
2024-03-13 14:13:22 +0900

    practice

---
+++ chap02/pages.js
@@ -0,0 +1,5 @@
+let sc = 31;
+let ps = 10;
+
+let pages = Math.ceil(sc / ps);
+console.log(pages);
---
+++ chap02/random.js
@@ -0,0 +1,3 @@
+for(let i = 0; i < 30; ++i){
+    console.log(Math.floor(Math.random() * 11) + 10);
+}
